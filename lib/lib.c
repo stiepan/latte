a75c490804f768e5992dcedce1bf0c43da7d1245
stiepan <kamiltokarski04@gmail.com>
2020-01-06 00:03:53 +0100

    adding necessary runtime

---
+++ lib/lib.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char * const EMPTY_STR = "";
+
+void printInt(int n) {
+    printf("%d\n", n);
+}
+
+void printString(char * s) {
+    printf("%s\n", s);
+}
+
+int readInt() {
+    int n;
+    scanf("%d", &n);
+    return n;
+}
+
+char * readString() {
+    char * line = NULL;
+    size_t size;
+    if (getline(&line, &size, stdin) == -1) {
+        return EMPTY_STR;
+    }
+    return line;
+}
+
+char * __concat(char * s1, char * s2) {
+    char *result = malloc(strlen(s1) + strlen(s2) + 1);
+    strcpy(result, s1);
+    strcat(result, s2);
+    return result; 
+}
+
+int __strEq(char * s1, char * s2) {
+    return strcmp(s1, s2) == 0; 
+}
+
+void error() {
+
+}
